7286bc937e8e8d3cd2f1465d0efdcf4d938e6eb6
tony-pitchin <131126080+tony-pitchin@users.noreply.github.com>
2023-08-30 10:20:44 +0800

    Update main.tf
    Added group naming

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ resource "azurerm_resource_group" "rg_example" {
 resource "azurerm_virtual_network" "vn_example" {
   name                = "example-virtual-network"
   address_space       = ["10.0.0.0/16"]
-  location            = rg_example.location
-  resource_group_name = rg_example.name
+  location            = azurerm_resource_group.rg_example.location
+  resource_group_name = azurerm_resource_group.rg_example.name
 }
 
 resource "azurerm_subnet" "sb_example" {
   name                 = "example-subnet"
-  resource_group_name  = rg_example.name
-  virtual_network_name = vn_example.name
+  resource_group_name  = azurerm_resource_group.rg_example.name
+  virtual_network_name = azurerm_virtual_network.vn_example.name
   address_prefixes     = ["10.0.1.0/24"]
 
   delegation {
@@ -32,17 +32,17 @@ resource "azurerm_subnet" "sb_example" {
 
 resource "azurerm_service_plan" "sp_example" {
   name                = "example-serviceplan" 
-  resource_group_name = rg_example.name
-  location            = rg_example.location
+  resource_group_name = azurerm_resource_group.rg_example.name
+  location            = azurerm_resource_group.rg_example.location
   os_type             = "Windows"
   sku_name            = "S1"
 }
 
 resource "azurerm_linux_web_app" "app_example" {
   name                = "my-unique-appname"
-  location            = rg_example.location
-  resource_group_name = rg_example.name
-  service_plan_id     = sp_example.id
+  location            = azurerm_resource_group.rg_example.location
+  resource_group_name = azurerm_resource_group.rg_example.name
+  service_plan_id     = azurerm_service_plan.sp_example.id
 
   site_config {}
 
@@ -53,13 +53,13 @@ resource "azurerm_linux_web_app" "app_example" {
 
 resource "azurerm_linux_web_app_slot" "example-staging" {
   name           = "example-slot"
-  app_service_id = azurerm_linux_web_app.example.id
+  app_service_id = azurerm_linux_web_app.app_example.id
 
   site_config {}
 }
 
 resource "azurerm_app_service_slot_virtual_network_swift_connection" "example" {
-  slot_name      = example-staging.name
-  app_service_id = app_example.id
-  subnet_id      = sb_example.id
+  slot_name      = azurerm_linux_web_app_slot.example-staging.name
+  app_service_id = azurerm_linux_web_app.app_example.id
+  subnet_id      = azurerm_subnet.sb_example.id
 }
